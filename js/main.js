dd28a052f7f62bd4d045b04f5734e831b1e9ba17
tallsara <sarawasserbo@gmail.com>
2020-12-14 13:38:45 -0500

    Add submit button

--- js/main.js
+++ js/main.js
@@ -1,53 +1,7 @@
-const toggleCollapse = document.querySelector('.toggle-collapse span');
-const nav = document.querySelector('.nav')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
 
-
-//on click event on toggle Collapse span tag
-toggleCollapse.onclick = () =>{
-  nav.classList.toggle('collapse');
-}
-
-window.addEventListener("DOMContentLoaded", function() {
-
-    // get the form elements defined in your form HTML above
-
-    var form = document.getElementById("my-form");
-    //var button = document.getElementById("my-form-button");
-    var status = document.getElementById("status");
-
-    // Success and Error functions for after the form is submitted
-
-    function success() {
-      form.reset();
-      status.innerHTML = "Thanks!";
-    }
-
-    function error() {
-      status.innerHTML = "Oops! There was a problem.";
-    }
-
-    // handle the form submission event
-
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
-  });
-
-  // helper function for sending an AJAX request
-
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
-  }
+menu.addEventListener('click', function () {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+})
